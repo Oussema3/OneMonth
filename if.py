be7310282d57360436781d513e46031922b13f07
garrech1 <garrech1@gmail.com>
2021-05-30 18:35:54 +0100

    simple condition

---
+++ if.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+answer = (input("do u wanna hear a joke ? "))
+answer = answer.upper()
+if answer == "YES":
+    print("I'am against picketing, but I don't know how to  show it .")
+    
