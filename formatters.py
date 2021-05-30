1a5bec02cfa2e0b8e17f2b9edb83b6d5cf7bebae
garrech1 <garrech1@gmail.com>
2021-05-30 17:00:11 +0100

    using variables in strings

---
+++ formatters.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#strings are texts surronded by quotes singles '' or double "" or triple """
+kanye_quote = "My greatest pain in life is that i will never be able to see my self perform live"
+print(kanye_quote)
+print()
+print("Now splited")
+print()
+kanye_splited_quote = """My greatest pain in life
+is that i will never be able to
+see my self perform live """
+print(kanye_splited_quote)
+hamilton_quote = "well , the word got arround, they said, \"this kid is insane\""
+print()
+print(hamilton_quote)
+
+name = "Mathan Griffel"
+orphan_fee = 200
+teddy_bear_fee = 121.087525
+
+total = orphan_fee + teddy_bear_fee
+
+#print(name, "the total will be: ", total)
+print()
+print(f"{name} the total will be {total:.2f}")
