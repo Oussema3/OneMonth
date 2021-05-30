083446dd98b48da0d1c310440b7810b2187d6cff
garrech1 <garrech1@gmail.com>
2021-05-30 16:41:46 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#strings are texts surronded by quotes singles '' or double "" or triple """
+kanye_quote = "My greatest pain in life is that i will never be able to see my self perform live"
+print(kanye_quote)
+print()
+print("Now splited")
+print()
+kanye_splited_quote = """My greatest pain in life 
+is that i will never be able to 
+see my self perform live """
+print(kanye_splited_quote)
+hamilton_quote = "well , the word got arround, they said, \"this kid is insane\""
+print()
+print(hamilton_quote)
