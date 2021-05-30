f543ddbf1a4a3652011f9477fcfc45837d0e3609
garrech1 <garrech1@gmail.com>
2021-05-30 17:44:32 +0100

    User Input

---
+++ Input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+name = input("whats your name ? ")
+age =int(input("whats your age ? "))
+
+print(f"hello {name}")
+if age < 18:
+    print(f"your are young {name}")
+else :
+    print(f"you are old enough {name}")
