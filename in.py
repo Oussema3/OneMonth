79e7a51a5e3be009ea5760a04d19f51b686c45bd
garrech1 <garrech1@gmail.com>
2021-05-31 08:15:13 +0100

    about in

--- in.py
+++ in.py
@@ -9,4 +9,18 @@ elif answer in negative_answers:
     print("Fine :( ")
 else:
     print("i dont understand")
-    
+#another way of doing that     
+"""
+answer = input("Do you want to hear a joke? ")
+
+affirmative_responses = ["yes", "y"]
+negative_responses = ["no", "n"]
+
+if answer.lower() in affirmative_responses:
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedburg (RIP)
+elif answer.lower() in negative_responses:
+    print("Fine.")
+else:
+    print("I don't understand.")
+"""
