23fabec4d6d5d5fdee47de4b21f6139c7c73830d
garrech1 <garrech1@gmail.com>
2021-05-31 10:22:25 +0100

    still in lists

---
+++ lists2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+random_things = ["Puppies", 55, "Anthony Weiner", 1/2, ["OH, NO !!", "A list inside of a list"]]
+animals = ["bear", "tiger", "penguin", "zebra"]
+first_animal =animals[0]
+print("number of animals in the list is : ",len(animals))
+#this will store the last element of the list in a list called "another_list"
+another_list = random_things[-1]
+print(type(another_list))
+print()
+print(another_list)
