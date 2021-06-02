4188b3998ea75fbc1a1eb13ed95bc2811e81466b
garrech1 <garrech1@gmail.com>
2021-06-02 04:18:20 +0100

    2 simple functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def greet(name):
+    return f"hello {name}"
+
+print(greet("Oussema"))
+print(greet("Khaled"))
+
+def concatinating(word_one, word_two):
+    return word_one + word_two
+print()
+print(concatinating("Hey","Oussema"))
+
+
+def age_in_dog_years(age):
+    result = age * 7 
+    return result
+print()
+print(age_in_dog_years(15))
