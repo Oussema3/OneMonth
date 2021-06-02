b4b530ac0002c0710458dfa1a80921736cf0d9ee
garrech1 <garrech1@gmail.com>
2021-06-02 04:59:54 +0100

    little challeng !

---
+++ functionChallenge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""  ____________CHALLENGE_____________ """
+
+
+""" Create a function called uppercase_and_reverse that takes a little bit of text,
+    uppercases it all, and then reverses it (flips all the letters around) so that: """
+
+def uppercase_and_reverse(name):
+    result = name[::-1]
+    result_2 = result.upper()
+    return result_2
+
+print(uppercase_and_reverse("Do not go gentle into that good night."))
