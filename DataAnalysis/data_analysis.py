07a1f0c5ef8b974b25ff64692136aa0f4ee9a4d7
garrech1 <garrech1@gmail.com>
2021-06-03 19:01:14 +0100

    exploring data

---
+++ DataAnalysis/data_analysis.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import pandas as pd
+names = ['id','title','year','rating','votes','length','genres']
+data = pd.read_csv('imdb_top_10000.txt', sep="\t", names=names, index_col=0)
+#this line will prnt out all the data
+data
+#this function should print out the first column from the data that we have 
+data.head()
+
+""" this will show the first 3 colmn """
+data.head(3)
+
+# this line will print out the last 5 comlumns from our data
+
+data.tail()
+
+# go throuth the data and tel you for each comlumn how many values you have there and what type each column is 
+data.info()
+
+# go throuth the data and gives a lot of usefull discription to the data 
+data.describe()
+
